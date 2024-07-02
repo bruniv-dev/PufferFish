28aa0dd2008a99380f387689651df12f282f7eab
Nivedha R <bruniv.dev@gmail.com>
2024-07-02 22:16:38 +0530

    Add files via upload

---
+++ PufferFish/app.js
@@ -0,0 +1,17 @@
+// document
+//   .getElementById("toggle-password")
+//   .addEventListener("click", function () {
+//     const passwordField = document.getElementById("password");
+//     const passwordFieldType = passwordField.getAttribute("type");
+//     const eyeIcon = this.querySelector("img");
+
+//     if (passwordFieldType === "password") {
+//       passwordField.setAttribute("type", "text");
+//       eyeIcon.setAttribute("src", "./eye-icon-closed.png"); // Change to closed eye icon
+//       eyeIcon.setAttribute("alt", "Hide Password");
+//     } else {
+//       passwordField.setAttribute("type", "password");
+//       eyeIcon.setAttribute("src", "./eye-icon.png"); // Change back to open eye icon
+//       eyeIcon.setAttribute("alt", "Show Password");
+//     }
+//   });
